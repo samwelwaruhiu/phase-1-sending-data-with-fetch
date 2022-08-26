55b1a2c17e1d86571c14bce95de944440c3a0da3
Samwel Waruhiu <samweliwaruhiu@gmail.com>
2022-08-26 15:52:41 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(nameofuser, emailaddress) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json",
+        "Accept" : "application/json"
+      },
+      body: JSON.stringify({
+        name: nameofuser,
+        email : emailaddress
+
+      }
+    )})
+    .then(response => response.json())
+    .then(data => {
+        document.querySelector("form").append(data)
+    })
+    .catch(error => {
+        alert(`error`);
+        console.log(error.message);
+        document.querySelector("form").append(error.message)
+    })
+  }
+
+  //submitData(""Steve","vghjvbuhb")
